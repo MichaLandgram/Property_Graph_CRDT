fecd21dc540873c20e922fd1bd460da82d2a94ef
u5YuFKs2KxhiVZdNk98W <landgram@rptu.de>
2025-12-17 09:53:24 +0100

    introduce Schema Interface

---
+++ MasterReactApp_ts/my-app/src/Helper/types_interfaces/schema.ts
@@ -0,0 +1,11 @@
+import { Policy, labelTypes, edgeLabelTypes, dataTypes, boolKeys } from '../types_interfaces/types';
+
+export interface Schema_Interface {
+    labelTypeValues: labelTypes[];
+    edgeLabelTypeValues: edgeLabelTypes[];
+    allowedConnectivity: Record<labelTypes, Record<labelTypes, edgeLabelTypes[]>>;
+    allowedNodePropeerties: Record<labelTypes, Record<boolKeys, Record<string, dataTypes>>>;
+    allowedEdgeProperties: Record<edgeLabelTypes, Record<boolKeys, Record<string, dataTypes>>>;
+    policyValues: Policy[];
+    matchPolicyToLabelType(label: labelTypes): Policy;
+}
