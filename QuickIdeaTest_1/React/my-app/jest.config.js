6d16b5ec366c7241e6614caa2e19bb3a385ce0c8
Landgram <michael.david-landgraf@web.de>
2026-01-15 01:33:47 +0100

    clean-up-quick-test

--- QuickIdeaTest_1/React/my-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Allow transforming ESM modules inside these packages (yjs and lib0 use ESM files) - I hate this XD
-  transformIgnorePatterns: ['node_modules/(?!(yjs|lib0|sucrase)/)'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
-  },
-  // Setup file to polyfill `crypto` in the test environment when needed
-  setupFiles: ['<rootDir>/jest.setup.js'],
-};
